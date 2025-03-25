9d43ad518ccf04785e27878d0488445e8746174d
Corentin-Poulet <corentin.poulet@eleves.ec-nantes.fr>
2025-03-25 09:48:56 +0100

    Ajout du NavHost

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,14 +4,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.myapplication.ui.theme.Accueil
 import com.example.myapplication.ui.theme.MyApplicationTheme
+
+enum class AppliScreen(@StringRes val title: Int) {
+    Start(title = R.string.app_name),
+    Revision(title = R.string.app_name)
+}
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +44,48 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                Scaffold(modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    NavHost(
+                        navController = rememberNavController(),
+                        startDestination = AppliScreen.Start.name,
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    ) {
+                        composable(route = AppliScreen.Start.name) {
+                            Accueil(
+                                modifier = Modifier.padding(innerPadding).background(
+                                    color = Color(
+                                        alpha = 255,
+                                        red = 46,
+                                        green = 58,
+                                        blue = 98
+                                    )
+                                )
+                            )
+                        }
+                        /*composable(route = AppliScreen.Revision.name) {
+                            val context = LocalContext.current
+                            SelectOptionScreen(
+                                subtotal = uiState.price,
+                                options = DataSource.flavors.map { id ->
+                                    context.resources.getString(
+                                        id
+                                    )
+                                },
+                                onCancelButtonClicked = {
+                                    cancelOrderAndNavigateToStart(
+                                        viewModel,
+                                        navController
+                                    )
+                                },
+                                onSelectionChanged = { viewModel.setFlavor(it) },
+                                onNextButtonClicked = { navController.navigate(CupcakeScreen.Pickup.name) },
+                                modifier = Modifier.fillMaxHeight()
+                            )
+                        }*/
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/myapplication/ui/theme/Accueil.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Accueil.kt
@@ -1,5 +1,7 @@
 package com.example.myapplication.ui.theme
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -7,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -16,16 +20,20 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myapplication.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AcceuilAppBar(
     modifier:Modifier=Modifier
-) {
-    TopAppBar(
+) {TopAppBar(
         title = { Text("Accueil") },
         colors = TopAppBarDefaults.mediumTopAppBarColors(
             containerColor = MaterialTheme.colorScheme.primaryContainer
@@ -37,31 +45,84 @@ fun AcceuilAppBar(
 }
 
 @Composable
-fun Accueil(modifier: Modifier=Modifier){
-    Column(modifier=Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,horizontalAlignment = Alignment.CenterHorizontally){
+fun Accueil(modifier: Modifier=Modifier.background(color= Color(alpha = 255,red=46, green=58,blue=98))){
+    Column(modifier=Modifier.fillMaxSize().background(color= Color(alpha = 255,red=46, green=58,blue=98)), verticalArrangement = Arrangement.Center,horizontalAlignment = Alignment.CenterHorizontally){
         Row(horizontalArrangement = Arrangement.Center, modifier=modifier) {
             Button(
-                modifier = Modifier,
+                modifier = Modifier.width(200.dp),
                 onClick = {}
             ) {
-                Text("Créer sa propre fiche")
+                Text("Créer sa propre fiche", fontSize = 24.sp)
             }
         }
         Column(modifier=Modifier.fillMaxWidth().padding(20.dp),horizontalAlignment = Alignment.Start){
             Row(modifier=Modifier.fillMaxWidth()){
-                Text("Ma liste", modifier=modifier)
+                Text("liste 1", modifier=modifier,color=Color.White, fontSize = 28.sp)
                 Row(modifier=Modifier.fillMaxWidth(),horizontalArrangement = Arrangement.End){
                     Button(modifier=Modifier,onClick={}){
-
+                        Image(painter= painterResource(R.drawable.truc),
+                            contentDescription=null,
+                            contentScale= ContentScale.Fit,
+                            alpha=0.5F,
+                            modifier=Modifier.size(32.dp))
                     }
                     Button(modifier=Modifier,onClick={}){
-
+                        Image(painter= painterResource(R.drawable.watch),
+                            contentDescription=null,
+                            contentScale= ContentScale.Fit,
+                            alpha=0.5F,
+                            modifier=Modifier.size(32.dp)
+                        )
                     }
                     Button(modifier=Modifier,onClick={}){
-
+                        Image(painter= painterResource(R.drawable.partage),
+                            contentDescription=null,
+                            contentScale= ContentScale.Fit,
+                            alpha=0.5F,
+                            modifier=Modifier.size(32.dp))
                     }
                     Button(modifier=Modifier,onClick={}){
-
+                        Image(painter= painterResource(R.drawable.delete),
+                            contentDescription=null,
+                            contentScale= ContentScale.Fit,
+                            alpha=0.5F,
+                            modifier=Modifier.size(32.dp))
+                    }
+                }
+            }
+            Row(modifier=Modifier.fillMaxWidth()){
+                Text("liste 2", modifier=modifier,color=Color.White, fontSize = 28.sp)
+                Row(modifier=Modifier.fillMaxWidth(),horizontalArrangement = Arrangement.End){
+                    Button(modifier=Modifier,onClick={}){
+                        Image(painter= painterResource(R.drawable.truc),
+                            contentDescription=null,
+                            contentScale= ContentScale.Fit,
+                            alpha=0.5F,
+                            modifier=Modifier.size(32.dp)
+                            )
+                    }
+                    Button(modifier=Modifier,onClick={}){
+                        Image(painter= painterResource(R.drawable.watch),
+                            contentDescription=null,
+                            contentScale= ContentScale.Fit,
+                            alpha=0.5F,
+                            modifier=Modifier.size(32.dp)
+                        )
+                    }
+                    Button(modifier=Modifier,onClick={}){
+                        Image(painter= painterResource(R.drawable.partage),
+                            contentDescription=null,
+                            contentScale= ContentScale.Fit,
+                            alpha=0.5F,
+                            modifier=Modifier.size(32.dp))
+                    }
+                    Button(modifier=Modifier,onClick={}){
+                        Image(painter= painterResource(R.drawable.delete),
+                            contentDescription=null,
+                            contentScale= ContentScale.Fit,
+                            alpha=0.5F,
+                            modifier=Modifier.size(32.dp)
+                            )
                     }
                 }
             }
