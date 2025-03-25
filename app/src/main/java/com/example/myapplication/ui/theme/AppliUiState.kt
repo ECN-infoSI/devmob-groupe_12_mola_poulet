ccb498c909c288022442b80bb1ceefcd8f27fdd3
Ivymushroom <molaarman5@gmail.com>
2025-03-25 11:37:04 +0100

    modif en data class et ajout de data

--- app/src/main/java/com/example/myapplication/ui/theme/AppliUiState.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/AppliUiState.kt
@@ -1,4 +1,11 @@
 package com.example.myapplication.ui.theme
 
-class AppliUiState {
+data class AppliUiState(
+    val currentFlashCardName: String = "",
+    val currentQuestionAnswer: Pair<String, String> = Pair("",""),
+    val currentAnswerCount: Int = 1,
+    val score: Int = 0,
+    val isGuessedAnswerWrong: Boolean = false,
+    val isRevisionOver: Boolean = false
+){
 }
